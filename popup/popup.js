6df458b21bc1e14752cdf19b5a479940635c96fd
22-JWL <100843910+22-JWL@users.noreply.github.com>
2024-03-16 14:23:48 +0900

    feat: addEventListener

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,43 @@
 const images = Array.from(document.querySelectorAll("img"));
+const title = document.querySelector("h1.hello");
+
+title.innerText = "Hello from popup.js";
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseover", handleTitleMouseOver);
+title.addEventListener("mouseout", handleTitleMouseOut);
+
+// window.addEventListener("resize", handlewindowResize);
+window.addEventListener("copy", handlewindowCopy);
+window.addEventListener("offline", handelOffline);
+window.addEventListener("online", handelOnline);
+
+function handelOffline() {
+  console.log("out of service!");
+}
+
+function handelOnline() {
+  console.log("we are back!");
+}
+
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+
+function handleTitleMouseOver() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleTitleMouseOut() {
+  title.innerText = "Mouse is gone!";
+}
+
+// function handlewindowResize() {
+//   document.body.style.backgroundColor = "tomato";
+// }
+function handlewindowCopy() {
+  alert("copied!");
+}
 
 images.forEach((img) => {
   img.addEventListener("click", () => {
